1840b44f277450a77551cd98a6e9a0f12d34a34f
Bhavye <bhavyedevelopment2003@gmail.com>
2023-12-28 08:57:19 +0530

    basic python ide implemented

---
+++ src/app.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const utils = require('./utils/utils')
+const fs = require('fs')
+
+const app = express()
+
+app.set("view engine", "ejs")
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.get("/", (req, res) => {
+    utils.initDockerImage().then(containerId => {
+        // console.log(`id: ${containerId}`)
+        return res.redirect(`/playground?id=${containerId}`)
+    }).catch(err => {
+        console.log(`exec error: ${err}`)
+        return res.json({ msg: err })
+    })
+})
+
+app.get("/playground", (req, res) => {
+    const containerId = req.query.id
+    const output = req.query.output
+    console.log(req.query)
+    res.render("playground", { containerId, output })
+})
+
+app.post("/playground", (req, res) => {
+    const userCode = req.body.code
+    const containerId = req.query.id
+
+    utils.writeToFile(userCode, "./code/main.py").then(() => {
+
+        utils.runCode(containerId, "main.py").then(output => {
+            utils.writeToFile(output, "./output/output.txt")
+            return res.redirect(`/playground?id=${containerId}&output=${output}`)
+        })
+
+    }).catch(error => {
+        console.log(error)
+    })
+})
+
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000...")
+})
---
+++ src/utils/utils.js
@@ -0,0 +1,57 @@
+const fs = require('fs')
+// const util = require('node:util');
+// const exec = util.promisify(require('node:child_process').exec);
+const { exec } = require('node:child_process')
+
+async function writeToFile(code, fileNmae) {
+    return /** @type {Promise<void>} */(new Promise((resolve, reject) => {
+        fs.writeFile(`${fileNmae}`, code, (err) => {
+            if (err) {
+                reject(`error: ${err}`)
+                return
+            }
+            else {
+                console.log(`written file successfully -> ${fileNmae}`)
+                resolve()
+                return
+            }
+        })
+    }))
+}
+
+
+async function initDockerImage() {
+    const command = "docker run -dtp 127.0.0.1:3000:3000 code-runner bash"
+
+    // initialise container and return id
+    return executeCommand(command)
+}
+
+
+async function runCode(containerId, fileNmae) {
+    // copy file to docker container and get it's output
+    const command = `docker cp code/${fileNmae} ${containerId}:/code && docker exec ${containerId} python3 ${fileNmae}`
+    return executeCommand(command)
+}
+
+
+/**
+ * @param {string} command
+ */
+function executeCommand(command) {
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                reject(error)
+                return;
+            }
+            resolve(stdout)
+            // console.error(`stderr: ${stderr}`);
+        });
+    });
+}
+
+
+
+
+module.exports = { writeToFile, initDockerImage, runCode }
